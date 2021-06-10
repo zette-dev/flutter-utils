f8116da1e1b08909071edbb5208cbb69326d2aed
Nate Frechette <nate@dropsource.com>
2021-06-10 11:03:37 -0400

    optional onpressed

--- lib/src/ui/animated_card_navigator.dart
+++ lib/src/ui/animated_card_navigator.dart
@@ -5,7 +5,7 @@ class AnimatedTap extends StatefulWidget {
   AnimatedTap({
     Key? key,
     required this.child,
-    required this.onPressed,
+    this.onPressed,
     this.start = 0.0,
     this.end = 0.04,
     this.duration = const Duration(milliseconds: 70),
@@ -13,7 +13,7 @@ class AnimatedTap extends StatefulWidget {
     this.behavior = HitTestBehavior.opaque,
   }) : super(key: key);
   final Widget child;
-  final Function(BuildContext) onPressed;
+  final Function(BuildContext)? onPressed;
   final double start, end;
   final Duration duration;
   final bool giveHapticFeedback;
@@ -75,7 +75,7 @@ class _AnimatedTapState extends State<AnimatedTap> {
         // Animation is done and was not cancelled
         if (_end == 0 && !_cancelled) {
           if (widget.onPressed != null) {
-            widget.onPressed(context);
+            widget.onPressed!(context);
           }
         } else if (_end == widget.end && _released) {
           setState(() => _end = 0);
