d6ddde37d08252cd614555fe1509a34e0390a48e
Nate Frechette <nate@dropsource.com>
2021-07-28 12:08:20 -0400

    added type

--- lib/src/routing.dart
+++ lib/src/routing.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart' show BuildContext, Route;
 
-typedef DeepLinkHandler = Future<Route?> Function(
-    BuildContext ctx, Map<String, List<String>> params, dynamic manager),;
+typedef DeepLinkHandler<M> = Future<Route?> Function(
+  BuildContext ctx,
+  Map<String, List<String>> params,
+  M manager,
+);
