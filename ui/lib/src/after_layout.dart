157b3f03a470100e91a151376a1f06d8989d95a5
Nathan Frechette <>
2022-10-17 14:35:18 -0400

    updates

--- ui/lib/src/after_layout.dart
+++ ui/lib/src/after_layout.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => afterFirstLayout(context));
+  }
+
+  void afterFirstLayout(BuildContext context);
+}
+
+mixin AfterConsumerLayoutMixin<T extends ConsumerStatefulWidget> on ConsumerState<T> {
   @override
   void initState() {
     super.initState();
