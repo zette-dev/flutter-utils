eaab7a0c3efde8ff76c6eb995a6349561969e455
Nathan Frechette <>
2023-05-11 11:30:55 -0400

    numeric regex

--- utils/lib/src/formatters.dart
+++ utils/lib/src/formatters.dart
@@ -32,22 +32,19 @@ String? formatPhoneNumber(String? phoneNumber) {
     var areaCode = '';
     if (hasAreaCode) {
       const areaCodeLength = 3;
-      final areaCodeSubstring =
-          numbersOnly.substring(sourceIndex, areaCodeLength + sourceIndex);
+      final areaCodeSubstring = numbersOnly.substring(sourceIndex, areaCodeLength + sourceIndex);
       areaCode = '($areaCodeSubstring)';
       sourceIndex += areaCodeLength;
     }
 
     // Prefix, 3 characters
     const prefixLength = 3;
-    final prefix =
-        numbersOnly.substring(sourceIndex, prefixLength + sourceIndex);
+    final prefix = numbersOnly.substring(sourceIndex, prefixLength + sourceIndex);
     sourceIndex += prefixLength;
 
     // Suffix, 4 characters
     const suffixLength = 4;
-    final suffix =
-        numbersOnly.substring(sourceIndex, suffixLength + sourceIndex);
+    final suffix = numbersOnly.substring(sourceIndex, suffixLength + sourceIndex);
 
     return '$leadingOne$areaCode $prefix-$suffix';
   } else {
@@ -55,11 +52,16 @@ String? formatPhoneNumber(String? phoneNumber) {
   }
 }
 
-final RegExp _emailRegex = RegExp(
-    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+RegExp _emailRegex = new RegExp(
+    r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$");
 
-String? emailValidator(String? emailInput,
-    {bool isRequired = false, String errorMessage = 'Invalid Email Address'}) {
+final RegExp _numericRegex = RegExp(r'^-?[0-9]+$');
+
+bool isNumeric(String str) {
+  return _numericRegex.hasMatch(str);
+}
+
+String? emailValidator(String? emailInput, {bool isRequired = false, String errorMessage = 'Invalid Email Address'}) {
   if (!isRequired && (emailInput == null || emailInput.isEmpty)) {
     return null;
   }
