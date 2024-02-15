f5bfc6dcf4987505368f0ca28eed997207da6363
Nathan Frechette <>
2024-02-14 20:08:49 -0500

    networking

--- ui/lib/src/networking.dart
+++ ui/lib/src/networking.dart
@@ -1,4 +1,3 @@
-import 'package:dio/browser.dart';
 import 'package:dio/dio.dart';
 import 'package:ds_utils/ds_utils.dart';
 import 'package:flutter/foundation.dart';
@@ -13,41 +12,47 @@ final apiAuthHeaders = StateProvider<Map<String, String>>(
 );
 
 final dioClientProvider = Provider.family<Dio, String>(
-  (ref, baseUrl) => Dio()
-    ..options = BaseOptions(
-      baseUrl: 'https://$baseUrl',
-      contentType: 'application/json',
-      responseType: ResponseType.json,
-    )
-    ..httpClientAdapter = kIsWeb ? BrowserHttpClientAdapter() : NativeAdapter()
-    ..transformer = BackgroundTransformer()
-    ..interceptors.add(InterceptorsWrapper(onRequest: ((options, handler) {
-      final authHeaders = ref.read(apiAuthHeaders);
-      options.headers.addAll(
-        {
-          ...authHeaders,
-          'Request-Id': const Uuid().v4(),
-        },
-      );
-      return handler.next(options);
-    }), onError: ((DioException e, handler) async {
-      if (e.response != null) {
-        switch (e.response?.statusCode) {
-          case 401:
-            DioException(
-              requestOptions: e.requestOptions,
-              type: DioExceptionType.badResponse,
-              error: UnauthorizedRequestError(),
-              response: e.response,
-              message: e.message,
-              stackTrace: e.stackTrace,
-            );
-            break;
+  (ref, baseUrl) {
+    final client = Dio()
+      ..options = BaseOptions(
+        baseUrl: 'https://$baseUrl',
+        contentType: 'application/json',
+        responseType: ResponseType.json,
+      )
+      ..transformer = BackgroundTransformer()
+      ..interceptors.add(InterceptorsWrapper(onRequest: ((options, handler) {
+        final authHeaders = ref.read(apiAuthHeaders);
+        options.headers.addAll(
+          {
+            ...authHeaders,
+            'Request-Id': const Uuid().v4(),
+          },
+        );
+        return handler.next(options);
+      }), onError: ((DioException e, handler) async {
+        if (e.response != null) {
+          switch (e.response?.statusCode) {
+            case 401:
+              DioException(
+                requestOptions: e.requestOptions,
+                type: DioExceptionType.badResponse,
+                error: UnauthorizedRequestError(),
+                response: e.response,
+                message: e.message,
+                stackTrace: e.stackTrace,
+              );
+              break;
+          }
         }
-      }
 
-      return handler.reject(e);
-    }))),
+        return handler.reject(e);
+      })));
+    if (!kIsWeb) {
+      client.httpClientAdapter = NativeAdapter();
+    }
+
+    return client;
+  },
   dependencies: [
     apiAuthHeaders,
   ],
