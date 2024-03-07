6b36fe4d8c12cdb72b5272d072961344f48005d4
Nathan Frechette <>
2024-03-06 23:26:56 -0500

    remove compute

--- ui/lib/src/helpers.dart
+++ ui/lib/src/helpers.dart
@@ -1,18 +1,5 @@
-import 'dart:async';
-
-import 'package:flutter/foundation.dart' as flutter show compute;
-import 'package:flutter/foundation.dart' show ComputeCallback, defaultTargetPlatform, kIsWeb;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
 import 'package:flutter/widgets.dart';
-
-// WORKAROUND FOR NOW - isolates don't work with flutter driver
-// https://github.com/flutter/flutter/issues/24703
-Future<R> compute<Q, R>(ComputeCallback<Q, R> callback, Q message) async {
-  // if (flutter.kDebugMode) {
-  //   return callback(message);
-  // }
-
-  return await flutter.compute(callback, message);
-}
 
 bool isTablet(BuildContext context) {
   var shortestSide = MediaQuery.of(context).size.shortestSide;
