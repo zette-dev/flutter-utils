dc3a90b088b02c950908fdb1238ea050a10a5441
Nathan Frechette <>
2024-03-27 21:55:02 -0400

    updates

--- utils/lib/src/collection_extensions.dart
+++ utils/lib/src/collection_extensions.dart
@@ -155,21 +155,3 @@ extension FutureListExtension<T> on List<Future<List<T>>> {
         return _combined;
       });
 }
-
-bool mapEquals<T, U>(Map<T, U>? a, Map<T, U>? b) {
-  if (a == null) {
-    return b == null;
-  }
-  if (b == null || a.length != b.length) {
-    return false;
-  }
-  if (identical(a, b)) {
-    return true;
-  }
-  for (final T key in a.keys) {
-    if (!b.containsKey(key) || b[key] != a[key]) {
-      return false;
-    }
-  }
-  return true;
-}
