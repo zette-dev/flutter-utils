dbcc9c4469c2c75ba1ed3040e69734f087d34a7d
Nathan Frechette <>
2024-08-07 23:25:42 -0400

    uri path ext

--- utils/lib/src/file_extensions.dart
+++ utils/lib/src/file_extensions.dart
@@ -10,3 +10,14 @@ extension FileExtensions on File {
     return null;
   }
 }
+
+extension UriExtensions on Uri {
+  String? get fileExtension {
+    List<String> _path = path.split('.');
+    if (_path.isNotEmpty) {
+      return _path.last.toLowerCase();
+    }
+
+    return null;
+  }
+}
