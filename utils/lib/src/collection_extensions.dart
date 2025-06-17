b2a618a944f56039ffcec620fd7a6484ce960ba7
Nathan Frechette <>
2025-06-17 14:27:52 -0400

    deps

--- utils/lib/src/collection_extensions.dart
+++ utils/lib/src/collection_extensions.dart
@@ -31,19 +31,18 @@ extension MapMethods<K, T> on Map {
     return filtered;
   }
 
-  Map<K, T> filterOutNullsOrEmpty() {
-    final Map<K, T> filtered = <K, T>{};
-    forEach((key, value) {
-      if (value != null && value is! Map && value is! Iterable) {
-        filtered[key] = value;
-      } else if (value is Map<dynamic, dynamic> && value.isNotEmpty) {
-        filtered[key] = value as T;
-      } else if (value is Iterable && value.isNotEmpty) {
-        filtered[key] = value as T;
-      }
-    });
-    return filtered;
-  }
+  Map<K, T> filterOutNullsOrEmpty() => <K, T>{
+    for (var entry in entries)
+      if (entry.value != null && entry.value is! Map && entry.value is! Iterable) ...{
+        entry.key: entry.value,
+      } else if (entry.value is Map && (entry.value as Map).isNotEmpty) ...{
+        entry.key: entry.value as T,
+      } else if (entry.value is Iterable && (entry.value as Iterable).isNotEmpty) ...{
+        entry.key: entry.value as T,
+      } else if (entry.value is String && (entry.value as String).isNotEmpty) ...{
+        entry.key: entry.value as T,
+      },
+  };
 
   Map<K, T> sortKeys() {
     final _keys = keys.toList();
